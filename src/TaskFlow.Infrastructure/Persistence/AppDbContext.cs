573434663dfaf5a49f2dbc66e3edc0b2c55ac354
korolslava <sviatoslav.motalo@gmail.com>
2026-03-26 15:14:10 +0200

    chore(db): add AppDbContext with DbSets for all entities

---
+++ src/TaskFlow.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Domain.Entities;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Workspace> Workspaces => Set<Workspace>();
+    public DbSet<WorkspaceMember> Members => Set<WorkspaceMember>();
+    public DbSet<Project> Projects => Set<Project>();
+    public DbSet<Board> Boards => Set<Board>();
+    public DbSet<BoardColumn> Columns => Set<BoardColumn>();
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+    public DbSet<Sprint> Sprints => Set<Sprint>();
+    public DbSet<Comment> Comments => Set<Comment>();
+    public DbSet<ActivityLog> ActivityLogs => Set<ActivityLog>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
