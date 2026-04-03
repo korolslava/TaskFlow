6f298bf3ac0fec4c18f47b59ab2cd5692725e6e8
korolslava <sviatoslav.motalo@gmail.com>
2026-04-03 23:05:46 +0300

    feat(activity): add ActivityLogHandler for domain events

---
+++ src/TaskFlow.Application/Notifications/Handlers/ActivityLogHandler.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Domain.Events;
+using TaskFlow.Infrastructure.Persistence;
+
+namespace TaskFlow.Application.Notifications.Handlers;
+
+public class ActivityLogHandler(AppDbContext db)
+    : INotificationHandler<TaskMovedEvent>,
+      INotificationHandler<TaskCreatedEvent>,
+      INotificationHandler<CommentAddedEvent>
+{
+    public async Task Handle(TaskMovedEvent notification, CancellationToken ct)
+    {
+        var log = ActivityLog.Create(
+            Guid.Empty,
+            notification.MovedByUserId,
+            $"moved task to another column",
+            "TaskItem",
+            notification.TaskItemId);
+
+        db.ActivityLogs.Add(log);
+        await db.SaveChangesAsync(ct);
+    }
+
+    public async Task Handle(TaskCreatedEvent notification, CancellationToken ct)
+    {
+        var log = ActivityLog.Create(
+            Guid.Empty,
+            notification.CreatedByUserId,
+            "created a new task",
+            "TaskItem",
+            notification.TaskItemId);
+
+        db.ActivityLogs.Add(log);
+        await db.SaveChangesAsync(ct);
+    }
+
+    public async Task Handle(CommentAddedEvent notification, CancellationToken ct)
+    {
+        var log = ActivityLog.Create(
+            Guid.Empty,
+            notification.AuthorId,
+            "added a comment",
+            "Comment",
+            notification.CommentId);
+
+        db.ActivityLogs.Add(log);
+        await db.SaveChangesAsync(ct);
+    }
+}
