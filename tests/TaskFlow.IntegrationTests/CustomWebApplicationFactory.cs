270e5df7c7f73e9c5cd6855cdbcdc732b44786b3
korolslava <sviatoslav.motalo@gmail.com>
2026-04-05 00:43:53 +0300

    test(integration): add CustomWebApplicationFactory with TestContainers

---
+++ tests/TaskFlow.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using TaskFlow.Infrastructure.Persistence;
+using Testcontainers.MsSql;
+using Xunit;
+
+namespace TaskFlow.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>,
+    IAsyncLifetime
+{
+    private readonly MsSqlContainer _sqlContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithPassword("TaskFlow_Test_Pass123!")
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _sqlContainer.StartAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _sqlContainer.StopAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d =>
+                d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_sqlContainer.GetConnectionString()));
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.Migrate();
+        });
+    }
+}
